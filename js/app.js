0dba7c81ffe019011b0640141016c2204504db33
unknown <mahmodulshahin665@gmail.com>
2022-05-23 11:02:17 +0600

    load openweathermap api and display some info

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
 const API_KEY = `f77a70fc22db25210b61e819bcebce52`;
 const searchTempetature = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY
-        }`;
+    const cityInput = document.getElementById('city-name-input').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
         .then(data => diplayTemperature(data));
 }
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 const diplayTemperature = temperature => {
+    setInnerText('city-name', temperature.name);
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('weatherCondition', temperature.weather[0].main)
+
+    //set weather icon
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+
+    const imgIcon = document.getElementById('weatherIcon');
+    imgIcon.setAttribute('src', url)
+
     console.log(temperature);
 }
